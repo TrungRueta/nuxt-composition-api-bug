befc8b05f1b2e9c71ac435a637a21d869c85ca17
TrungRueta <trungrueta@icloud.com>
2021-09-21 20:31:06 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { ref } from "@nuxtjs/composition-api";
+
+const a = ref(true);
+</script>
+
+<template>
+  <div>
+    <Test />
+
+    {{ a }}
+  </div>
+</template>
